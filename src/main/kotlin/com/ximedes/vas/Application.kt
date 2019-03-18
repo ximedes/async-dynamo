450a70ad645afda51bbd3a690eedde1719def7f4
gijsterhorst <gijs.ter.horst@ximedes.com>
2019-03-18 12:32:39 +0100

    work

--- src/main/kotlin/com/ximedes/vas/Application.kt
+++ src/main/kotlin/com/ximedes/vas/Application.kt
@@ -1,13 +1,16 @@
 package com.ximedes.vas
 
+import com.ximedes.vas.dynamo.dsl.writeTransaction
 import com.ximedes.vas.dynamo.initTables
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.ContentNegotiation
 import io.ktor.jackson.jackson
+import io.ktor.request.receive
 import io.ktor.response.respondText
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.routing
 import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
@@ -17,13 +20,18 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
+data class NewAccountMessage(val account: String
+                             , val user: String
+                             , val overdraft: Int
+                             , val description: String)
+
 fun Application.module() {
     val logger = KotlinLogging.logger {}
 
     val client = DynamoDbAsyncClient.builder()
-        .credentialsProvider(DefaultCredentialsProvider.create())
-        .region(Region.EU_WEST_1)
-        .build()
+            .credentialsProvider(DefaultCredentialsProvider.create())
+            .region(Region.EU_WEST_1)
+            .build()
 
     runBlocking {
         initTables(client)
@@ -36,6 +44,27 @@ fun Application.module() {
     routing {
         get("/") {
             call.respondText { "Hello, World" }
+        }
+        post("/account") {
+            val newAccountMessage = call.receive<NewAccountMessage>()
+            client.writeTransaction {
+                put("ledger") {
+                    item {
+                        "pk" from "USER-${newAccountMessage.user}"
+                    }
+//                    item {
+//                        "sk" from "ACC-${newAccountMessage.account}"
+//                    }
+//                    item {
+//                        "overdraft" from newAccountMessage.overdraft
+//                    }
+//                    item {
+//                        "description" from newAccountMessage.description
+//                    }
+                }
+
+            }
+
         }
     }
 }
--- src/main/kotlin/com/ximedes/vas/dynamo/DDL.kt
+++ src/main/kotlin/com/ximedes/vas/dynamo/DDL.kt
@@ -6,23 +6,16 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient
 import software.amazon.awssdk.services.dynamodb.model.KeyType
 import software.amazon.awssdk.services.dynamodb.model.ScalarAttributeType
 
+
 suspend fun initTables(client: DynamoDbAsyncClient) {
 
     val tables = client.listTables().await().tableNames()
 
-    if ("account" !in tables) {
-        client.createTable("account") {
-            attribute("account_id", ScalarAttributeType.S, KeyType.HASH)
-            throughput(5, 5)
+    if ("ledger" !in tables) {
+        client.createTable("ledger") {
+            attribute("pk", ScalarAttributeType.S, KeyType.HASH)
+            attribute("sk", ScalarAttributeType.S, KeyType.RANGE)
+            throughput(readCapacityUnits = 10, writeCapacityUnits = 10)
         }
     }
-
-    if ("transaction" !in tables) {
-        client.createTable("transaction") {
-            attribute("transaction_id", ScalarAttributeType.S, KeyType.HASH)
-            attribute("timestamp", ScalarAttributeType.S, KeyType.RANGE)
-            throughput(5, 5)
-        }
-    }
-
 }
